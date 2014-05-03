a88f55ff4e3465739f663713881484ef1febfebb
Amber Feng <amber@stripe.com>
2014-05-02 20:26:44 -0700

    Slightly clean up d3 code, but not really.

--- templates/js/todidnt-history.js
+++ templates/js/todidnt-history.js
@@ -1,6 +1,15 @@
-var margin = {top: 20, right: 20, bottom: 30, left: Math.round(window.innerWidth * 0.09)},
-  width = window.innerWidth  * 0.8,
-  height = 500;
+// Constants
+
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: Math.round(window.innerWidth * 0.09)
+};
+
+var width = window.innerWidth * 0.8, height = 500;
+
+// Graph scale setup
 
 var x = d3.scale.ordinal()
   .rangeRoundBands([0, width], .1);
@@ -20,12 +29,33 @@ var getColor = function(name) {
 
 var xAxis = d3.svg.axis()
   .scale(x)
-    .orient("bottom");
+  .orient("bottom");
 
 var yAxis = d3.svg.axis()
   .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".2s"));
+  .orient("left")
+  .tickFormat(d3.format(".2s"));
+
+// Transform data
+
+var authors = PRELOADED_DATA['authors'];
+color.domain(authors);
+
+var data = PRELOADED_DATA['history'].map(function(d) {
+  var y0 = 0
+  var todos = color.domain().map(function(name) {
+    return {
+      name: name,
+      y0: y0,
+      y1: y0 += +(d[name] || 0)
+    };
+  });
+  var total = todos[todos.length - 1].y1;
+
+  return {date: d.Date, todos: todos, total: total};
+});
+
+// Graph creation
 
 var svg = d3.select("section.content").append("svg")
   .attr("width", '100%')
@@ -33,24 +63,7 @@ var svg = d3.select("section.content").append("svg")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var data = PRELOADED_DATA['history'];
-var authors = PRELOADED_DATA['authors'];
-
-color.domain(authors);
-
-data.forEach(function(d) {
-  var y0 = 0;
-  d.ages = color.domain().map(function(name) {
-    return {
-      name: name,
-      y0: y0,
-      y1: y0 += +(d[name] || 0)
-    };
-  });
-  d.total = d.ages[d.ages.length - 1].y1;
-});
-
-x.domain(data.map(function(d) { return d.Date; }));
+x.domain(data.map(function(d) { return d.date; }));
 y.domain([0, d3.max(data, function(d) { return d.total; })]);
 
 svg.append("g")
@@ -68,19 +81,21 @@ svg.append("g")
   .style("text-anchor", "end")
   .text("TODOs");
 
-var state = svg.selectAll(".state")
+var dates = svg.selectAll(".dates")
   .data(data)
   .enter().append("g")
   .attr("class", "g")
-  .attr("transform", function(d) { return "translate(" + x(d.Date) + ",0)"; });
+  .attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; });
 
-state.selectAll("rect")
-  .data(function(d) { return d.ages; })
+dates.selectAll("rect")
+  .data(function(d) { return d.todos; })
   .enter().append("rect")
   .attr("width", x.rangeBand())
   .attr("y", function(d) { return y(d.y1); })
   .attr("height", function(d) { return y(d.y0) - y(d.y1); })
   .style("fill", function(d) { return getColor(d.name); });
+
+// Legend
 
 var legend = svg.selectAll(".legend")
   .data(color.domain().slice().reverse())
